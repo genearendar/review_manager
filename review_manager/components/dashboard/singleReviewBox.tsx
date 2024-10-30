b6b602c698f8346277c4f0ab19b2a4e0d6e95ce7
Gene Arendarenko <Genearendar@gmail.com>
2024-10-30 22:05:47 +0000

    Update singleReviewBox

--- review_manager/components/dashboard/singleReviewBox.tsx
+++ review_manager/components/dashboard/singleReviewBox.tsx
@@ -1,7 +1,12 @@
+'use client';
 import { Review } from "@/lib/reviews";
 export default function SingleReviewBox({review}: {review: Review}) {
   return (
-  <div>SingleReviewBox
-
+  <div>
+    <h3>{review.body}</h3>
+    <p>Stars: {review.stars}</p>
+    <p>Reviewed by: {review.reviewedBy}</p>
+    <p>Source: {review.source}</p>
+    <p>Date: {review.date}</p>
   </div>);
 }
