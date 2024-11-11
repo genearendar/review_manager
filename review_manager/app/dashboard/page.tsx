cee9a6e28e14b9bbdd5750bcae45de116ce1f426
Gene Arendarenko <Genearendar@gmail.com>
2024-11-11 22:44:38 +0000

    Add redirect from /dashboard to /dashboard/reviews

--- review_manager/app/dashboard/page.tsx
+++ review_manager/app/dashboard/page.tsx
@@ -1,8 +1,5 @@
-import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
-import { createClient } from "@/utils/supabase/server";
-import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 
 export default async function ProtectedPage() {
-  return <h1>Protected page</h1>;
+  redirect("/dashboard/reviews");
 }
